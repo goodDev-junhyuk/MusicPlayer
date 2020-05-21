074338a2873d3cf25d074fee456e4445051d39e6
junhyuk <dejavukk1@gmail.com>
2020-05-21 23:36:35 +0900

    UI구현, 오토레이아웃.

---
+++ MusicPlayer/GenreButtonScreen.swift
@@ -0,0 +1,36 @@
+//
+//  GenreButtonScreen.swift
+//  MusicPlayer
+//
+//  Created by junhyuk on 2020/05/21.
+//  Copyright © 2020 junhyuk. All rights reserved.
+//
+
+import UIKit
+
+class GenreButtonScreen: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func genrenButtonTapped(_ sender: UIButton) {
+        
+        
+        
+        
+    }
+    
+    
+    @IBAction func stopButtonTapped(_ sender: UIButton) {
+        
+        
+    }
+    
+    @IBAction func nextButtonTapped(_ sender: UIButton) {
+    }
+    
+
+}
