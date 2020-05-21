578a86322b168faffeb54fb521e608a5a574b429
junhyuk <dejavukk1@gmail.com>
2020-05-21 23:55:34 +0900

    MusicPlayer를 import하여 간단한 음악재생.

--- MusicPlayer/GenreButtonScreen.swift
+++ MusicPlayer/GenreButtonScreen.swift
@@ -7,8 +7,11 @@
 //
 
 import UIKit
+import MediaPlayer
 
 class GenreButtonScreen: UIViewController {
+    
+    var musicPlayer = MPMusicPlayerController.applicationMusicPlayer
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,19 +21,40 @@ class GenreButtonScreen: UIViewController {
     
     @IBAction func genrenButtonTapped(_ sender: UIButton) {
         
-        
-        
-        
+        MPMediaLibrary.requestAuthorization { (status) in
+            
+            if status == .authorized {
+                self.playerGenre(genre: sender.currentTitle!)
+                
+            }
+        }
     }
-    
     
     @IBAction func stopButtonTapped(_ sender: UIButton) {
         
+        musicPlayer.stop()
         
     }
     
     @IBAction func nextButtonTapped(_ sender: UIButton) {
+        
+        musicPlayer.skipToNextItem()
+        
     }
     
-
+    func playerGenre(genre: String) {
+        
+        musicPlayer.stop()
+        
+        let query = MPMediaQuery()
+        let predicate = MPMediaPropertyPredicate(value: genre, forProperty: MPMediaItemPropertyGenre)
+        
+        query.addFilterPredicate(predicate)
+        
+        musicPlayer.setQueue(with: query)
+        musicPlayer.shuffleMode = .songs
+        musicPlayer.play()
+        
+    }
+    
 }
